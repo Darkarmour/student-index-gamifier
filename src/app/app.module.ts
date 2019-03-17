ee25be1bb70939e102b5a6ac51e273c83d525dcb
darkarmour <jpchennai462@gmail.com>
2019-03-17 15:04:13 +0530

    App changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule, MatToolbarModule, MatListModule, MatTableModule, MatButtonToggleModule } from '@angular/material';
+import { MatSidenavModule, MatToolbarModule, MatListModule, MatTableModule, MatButtonToggleModule, MatCardModule, MatButtonModule, MatIconModule, MatGridListModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -23,7 +23,11 @@ import { SetupComponent } from './setup/setup.component';
     MatToolbarModule,
     MatListModule,
     MatTableModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
