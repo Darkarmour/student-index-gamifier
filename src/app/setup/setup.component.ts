6ee5184cfe9952c5818435ec9aa27b42c63d44ee
darkarmour <jpchennai462@gmail.com>
2019-03-17 15:24:05 +0530

    indices change

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -8,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 
 export class SetupComponent implements OnInit {
 
+  indices: Array<Index> = [];
   constructor() { }
 
   ngOnInit() {
+    this.indices.push({
+      name: 'Social Index',
+      description: 'Trend on the social interactivity of the students',
+      addedSections: [{
+        name: 'Conduct',
+        point: 100,
+        behaviours: [{
+          name: 'Feedback',
+          point: 40
+        },
+        {
+          name: 'Absenteeism',
+          point: 60
+        },
+        ]
+      }, {
+        name: 'Co-Curricular',
+        point: 200,
+        behaviours: [{
+          name: 'Event participation',
+          point: 120
+        }, {
+          name: 'Club Membership',
+          point: 80
+        }]
+      }]
+    })
   }
 
+}
+
+export interface Index {
+  name: string,
+  description: string,
+  addedSections: Array<Section>
+}
+
+export interface Section {
+  name: string,
+  behaviours: Array<Behaviour>
+  point: number
+}
+
+export interface Behaviour { //NOTE These are predefined
+  name: string,
+  properties?: Array<any>,
+  point: number
 }
